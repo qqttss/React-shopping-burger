6e9df98e2464c250fbf9e757e3287598b3defd2d
thomas <quantongshen@gmail.com>
2020-05-09 19:15:27 -0400

    Add form handling

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,18 +2,49 @@ import React from 'react';
 
 import classes from './Input.css';
 
-const input = (props) => {
+const input = ( props ) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
 
-    switch (props.inputtype) {
-        case ('input'):
-            inputElement = <input className={classes.inputElement} {...props} />;
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
             break;
-        case ('textarea'):
-            inputElement = <textarea className={classes.inputElement} {...props} />;
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={inputClasses}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputClasses}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.inputElement} {...props} />;
+            inputElement = <input
+                className={inputClasses}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
     }
 
     return (
@@ -22,6 +53,7 @@ const input = (props) => {
             {inputElement}
         </div>
     );
+
 };
 
 export default input;
